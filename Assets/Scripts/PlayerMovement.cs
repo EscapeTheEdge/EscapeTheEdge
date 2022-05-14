80dc99c6af1c0f68a4a913e9d3c49dd8a852624f
Hubert Balcerzak <hubertbalc@gmail.com>
2022-05-14 15:05:56 +0200

    add simple movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,41 +1,70 @@
-using System.Net.NetworkInformation;
-using System.Collections;
-using System.Collections.Generic;
+using System;
+using System.ComponentModel;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    Rigidbody rb;
+    [SerializeField] float meshSize = 1f;
+    [SerializeField] Vector3 meshOffset;
     [SerializeField] float movementSpeed = 5f;
     [SerializeField] float jumpForce = 5f;
     [SerializeField] Transform groundCheck;
     [SerializeField] LayerMask ground;
 
+    private Boolean isMoving;
+    private Vector3 meshPosition;
+    private Vector3 goalMeshPosition;
+    private Vector3 worldPosition;
+    private Vector3 goalWorldPosition;
+    Rigidbody rb;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        rb.isKinematic = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
+        if (isMoving) Move();
+        else CheckInput();
+        // rb.velocity = new Vector3(horizontalInput*movementSpeed, rb.velocity.y, verticalInput*movementSpeed);
 
-        rb.velocity = new Vector3(horizontalInput*movementSpeed, rb.velocity.y, verticalInput*movementSpeed);
+        // if (Input.GetButtonDown("Jump") && IsPlayerTouchingGround()){
+        // Jump();
+        // }
+    }
 
-        if (Input.GetButtonDown("Jump") && IsPlayerTouchingGround()){
-            Jump();
+    private void CheckInput()
+    {
+        int horizontalInput = Math.Sign(Input.GetAxis("Horizontal"));
+        int verticalInput = Math.Sign(Input.GetAxis("Vertical"));
+
+
+        if (horizontalInput != 0) StartMove(new Vector3(horizontalInput, 0, 0));
+        if (verticalInput != 0) StartMove(new Vector3(0, 0, verticalInput));
+    }
+
+    private void StartMove(Vector3 direction)
+    {
+        goalMeshPosition = meshPosition + direction;
+        goalWorldPosition = (goalMeshPosition * meshSize) + meshOffset;
+        isMoving = true;
+    }
+
+    private void Move()
+    {
+        if (rb.transform.position == goalWorldPosition)
+        {
+            isMoving = false;
+            worldPosition = goalWorldPosition;
+            meshPosition = goalMeshPosition;
+            return;
         }
-    }
 
-
-    void Jump(){
-            rb.velocity += new Vector3(rb.velocity.x,jumpForce,rb.velocity.z);
-    }
-
-    bool IsPlayerTouchingGround(){
-        return Physics.CheckSphere(groundCheck.position, .1f,ground);
+        Vector3 movement = Vector3.MoveTowards(rb.transform.position, goalWorldPosition, 2f * Time.deltaTime);
+        rb.transform.position = movement;
     }
 }
