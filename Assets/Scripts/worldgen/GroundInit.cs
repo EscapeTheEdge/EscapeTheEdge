ba1cebb1da6b1672cc0ceeeaed429555057abf67
Hubert Balcerzak <hubertus248@gmail.com>
2022-06-12 00:39:12 +0200

    Feature/generate trees (#5)
    * add simple movement
    
    * animate movement
    
    * improve player input
    
    * add scene with generated map
    
    * add tree generator
    
    * fix merge conflicts
    
    * 🧙🏼  (#3)
    
    * 🧙🏼
    
    * Add wizard
    
    * 🧙🏼
    
    Co-authored-by: Aleksandra Cynk <aleksandra.cynk@C-PH7K9JY4MQ.chime.local>
    Co-authored-by: Paweł Kopel <pawel.kopel2@gmail.com>
    
    * player movement, map generation (#4)
    
    * add simple movement
    
    * animate movement
    
    * improve player input
    
    * add scene with generated map
    
    * fix merge conflicts
    
    * 🧙🏼  (#3)
    
    * 🧙🏼
    
    * Add wizard
    
    * 🧙🏼
    
    Co-authored-by: Aleksandra Cynk <aleksandra.cynk@C-PH7K9JY4MQ.chime.local>
    Co-authored-by: Paweł Kopel <pawel.kopel2@gmail.com>
    
    * Copy scene2
    
    * Cpoy scene2
    
    * add simple movement
    
    * animate movement
    
    * improve player input
    
    * add scene with generated map
    
    * fix merge conflicts
    
    * Fix names
    
    Co-authored-by: Aleksandra Cynk <ola.zn123@wp.pl>
    Co-authored-by: Aleksandra Cynk <aleksandra.cynk@C-PH7K9JY4MQ.chime.local>
    Co-authored-by: Paweł Kopel <pawel.kopel2@gmail.com>
    
    * add simple movement
    
    * animate movement
    
    * add scene with generated map
    
    * add tree generator
    
    Co-authored-by: Aleksandra Cynk <ola.zn123@wp.pl>
    Co-authored-by: Aleksandra Cynk <aleksandra.cynk@C-PH7K9JY4MQ.chime.local>
    Co-authored-by: Paweł Kopel <pawel.kopel2@gmail.com>

---
+++ Assets/Scripts/worldgen/GroundInit.cs
@@ -0,0 +1,33 @@
+using System.Linq;
+using UnityEngine;
+using Random = System.Random;
+
+public class GroundInit : MonoBehaviour, MapInitializer
+{
+    [SerializeField] private GameObject treeTemplate;
+    [SerializeField] private Vector3 treeOffset = new Vector3(0, 1, 0);
+
+    public void Initialize()
+    {
+        var random = new Random();
+        var numberOfTrees = random.Next(8);
+        var treePositions = Enumerable.Range(0, 20)
+            .ToList()
+            .OrderBy(_ => random.Next())
+            .Take(numberOfTrees);
+
+        foreach (var treePosition in treePositions)
+        {
+            var newTree = GameObject.Instantiate(treeTemplate, transform);
+            newTree.transform.localPosition = new Vector3(10f - treePosition, 0f, 0f) + treeOffset;
+        }
+    }
+
+    void Update()
+    {
+    }
+    
+    void Start()
+    {
+    }
+}
