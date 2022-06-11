c31fdb0112977d618b4931c0b6499e4922468844
Hubert Balcerzak <hubertus248@gmail.com>
2022-06-12 00:30:58 +0200

    player movement, map generation (#4)
    * add simple movement
    
    * animate movement
    
    * improve player input
    
    * add scene with generated map
    
    * fix merge conflicts
    
    * 🧙🏼  (#3)
    
    * 🧙🏼
    
    * Add wizard
    
    * 🧙🏼
    
    Co-authored-by: Aleksandra Cynk <aleksandra.cynk@C-PH7K9JY4MQ.chime.local>
    Co-authored-by: Paweł Kopel <pawel.kopel2@gmail.com>
    
    * Copy scene2
    
    * Cpoy scene2
    
    * add simple movement
    
    * animate movement
    
    * improve player input
    
    * add scene with generated map
    
    * fix merge conflicts
    
    * Fix names
    
    Co-authored-by: Aleksandra Cynk <ola.zn123@wp.pl>
    Co-authored-by: Aleksandra Cynk <aleksandra.cynk@C-PH7K9JY4MQ.chime.local>
    Co-authored-by: Paweł Kopel <pawel.kopel2@gmail.com>

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,41 +1,81 @@
-using System.Net.NetworkInformation;
-using System.Collections;
-using System.Collections.Generic;
+using System;
+using System.ComponentModel;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    Rigidbody rb;
+    [SerializeField] float meshSize = 1f;
+    [SerializeField] Vector3 meshOffset;
+    [SerializeField] private float moveDuration = 0.2f;
     [SerializeField] float movementSpeed = 5f;
     [SerializeField] float jumpForce = 5f;
     [SerializeField] Transform groundCheck;
     [SerializeField] LayerMask ground;
+    [SerializeField] AnimationCurve animation;
+
+    private Boolean isMoving;
+    private Vector3 meshPosition;
+    private Vector3 goalMeshPosition;
+    private Vector3 worldPosition;
+    private Vector3 goalWorldPosition;
+    private float moveStartTime;
+    Rigidbody rb;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        rb.isKinematic = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
+        if (isMoving) Move();
+        CheckInput();
+    }
 
-        rb.velocity = new Vector3(horizontalInput*movementSpeed, rb.velocity.y, verticalInput*movementSpeed);
+    public int GetProgress()
+    {
+        return (int)meshPosition.z;
+    }
 
-        if (Input.GetButtonDown("Jump") && IsPlayerTouchingGround()){
-            Jump();
+    private void CheckInput()
+    {
+        if (Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.A)) StartMove(new Vector3(-1, 0, 0));
+        if (Input.GetKeyUp(KeyCode.RightArrow) || Input.GetKeyUp(KeyCode.D)) StartMove(new Vector3(1, 0, 0));
+        if (Input.GetKeyUp(KeyCode.UpArrow) || Input.GetKeyUp(KeyCode.W)) StartMove(new Vector3(0, 0, 1));
+        if (Input.GetKeyUp(KeyCode.DownArrow) || Input.GetKeyUp(KeyCode.S)) StartMove(new Vector3(0, 0, -1));
+    }
+
+    private void StartMove(Vector3 direction)
+    {
+        if (isMoving) EndMove();
+        goalMeshPosition = meshPosition + direction;
+        goalWorldPosition = (goalMeshPosition * meshSize) + meshOffset;
+        isMoving = true;
+        moveStartTime = Time.time;
+    }
+
+    private void Move()
+    {
+        if (Time.time > moveStartTime + moveDuration)
+        {
+            EndMove();
+            return;
         }
+
+        float interpolation = (Time.time - moveStartTime) / moveDuration;
+
+        Vector3 movement = Vector3.Lerp(worldPosition, goalWorldPosition, animation.Evaluate(interpolation));
+        rb.transform.position = movement;
     }
 
-
-    void Jump(){
-            rb.velocity += new Vector3(rb.velocity.x,jumpForce,rb.velocity.z);
-    }
-
-    bool IsPlayerTouchingGround(){
-        return Physics.CheckSphere(groundCheck.position, .1f,ground);
+    private void EndMove()
+    {
+        isMoving = false;
+        rb.transform.position = goalWorldPosition;
+        worldPosition = goalWorldPosition;
+        meshPosition = goalMeshPosition;
     }
 }
