6904c2f94cf1f6403a53527825926f368dc4b064
Hubert Balcerzak <hubertbalc@gmail.com>
2022-06-15 21:14:55 +0200

    add path enemy

---
+++ Assets/Scripts/enemy/PathEnemy.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class PathEnemy : MonoBehaviour
+{
+    private float speed;
+    private Vector3 direction;
+    private float maxDistance;
+    private Vector3 initialPosition;
+
+    public void Initialize(float speed, Vector3 direction, float maxDistance)
+    {
+        this.speed = speed;
+        this.direction = direction;
+        this.maxDistance = maxDistance;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        initialPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((transform.position - initialPosition).magnitude > maxDistance)
+        {
+            Destroy(gameObject);
+        }
+
+        transform.Translate(direction * (Time.deltaTime * speed));
+    }
+}
